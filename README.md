edb8e77b4c7311caa92b9e170924ab67df6bca26
Guy Bridge <guy@wait.com.au>
2018-09-26 09:11:21 +0800

    added code to focus on first element in form

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Date: 26/09/2018
 Author: Guy Bridge // Guy@bridgeit.com.au
 Description: TreeHouse - Full Stack JavaScript Project 03 - Build an interactive form
+
---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+* Date: 26/09/2018
+* Author: Guy Bridge
+* Description: Main Script file for FSJS TreeHouse Project 03 
+*/
+
+// Store the form in a variable
+const form = document.querySelector(".form");
+
+// Focus on the first form element
+const name = document.getElementById("name");
+name.focus();
+
+form.addEventListener("", (e)=>{
+    
+});
